15440b7e6afca26d0e18102cacb7e2c3f2247b7e
Hector V <luciano3103@outlook.com>
2025-10-27 16:52:37 -0400

    Hopefully not breaking everything
    spawn networked cube

---
+++ Assets/SpawnCube.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpawnCube : MonoBehaviour
+{
+    public GameObject theCube = null;
+    private bool ONCEONLY = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.RawButton.A))
+        {
+            Debug.LogWarning("FIRED");
+            if (theCube != null && !ONCEONLY)
+            {
+                StartCoroutine(spawnCube());
+            }
+        }
+    }
+
+    public IEnumerator spawnCube()
+    {
+        ONCEONLY = true;
+        Instantiate(theCube, this.transform.position, Quaternion.Euler(0, 0, 0));
+        Debug.LogWarning("FIRED2");
+        yield return new WaitForSeconds(1);
+        ONCEONLY = false;
+        Debug.LogWarning("Refreshed");
+    }
+}
