5264e36060219dbef79d39f9571dfd20b438bf7a
Samuel Arminana <armi.sam99@gmail.com>
2025-11-03 17:02:06 -0500

    Combining Logic

--- Assets/Scripts/TrackableManager.cs
+++ Assets/Scripts/TrackableManager.cs
@@ -3,7 +3,7 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class TrackableManager : NetworkBehaviour
+public class TrackableManager : MonoBehaviour
 {
     
     public UnityEvent<Transform> onTrackableAdded;
